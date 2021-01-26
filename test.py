402c967478e329e8e85ef7dff4ed1427044a5276
Yutaro Nakayama <170808naka@gmail.com>
2021-01-26 23:02:12 +0900

    remove unnecessary files

--- test.py
+++
@@ -1,8 +0,0 @@
-import time                            # スリープを使うために必要
-#import chromedriver_binary 
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path='/home/kao/stockTrade/chromedriver')
-driver.get('https://www.google.com/')  # Googleを開く
-time.sleep(5)                          # 5秒間待機
-driver.quit()                          # ブラウザを閉じる
