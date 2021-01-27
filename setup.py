ee05111a7cf31b6e302a0bc43da9ab2050813fdc
Yutaro Nakayama <170808naka@gmail.com>
2021-01-27 23:55:18 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="colpire",
+  version='1.0',
+  description='過去の株価情報を基に分析を行うシステム',
+  author='Yutaro Nakayama',
+  url='https://github.com/NakayamaYutaro/copire',
+)
