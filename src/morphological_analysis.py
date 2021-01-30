ca2557a1130347dfca7da2979f714f4e31e30603
Yutaro Nakayama <170808naka@gmail.com>
2021-01-31 00:31:05 +0900

    modify morphological_analysis.py[morpholoogical_analysis_wakati]

--- src/morphological_analysis.py
+++ src/morphological_analysis.py
@@ -1,14 +1,29 @@
 import MeCab
 
-mecab_ipadic_neologd_path = ""
+# MeCabのデフォルト設定辞書を確認 [sudo update-alternatives --config mecab-dictionary]
 
-def morpholigical_analysis_mecab(sentence: str, dic_path: str) -> list:
-  mt = MeCab.Tagger(mecab_ipadic_neologd_path)
+default_tagger_arg = "" 
+default_dictionary_path = ""
+
+def morphological_analysis_default(sentence: str, dic_path: str = None) -> list:
+  mt = MeCab.Tagger(default_tagger_arg)
   return mt.parse(sentence)
+
+def morphological_analysis_wakati(sentence: str, dic_path: str = None) -> list:
+  mt = MeCab.Tagger("-Owakati")
+  return mt.parse(sentence).split()
 
 def clean_mecab_sentence(sentence: str):
   word_list = []
-  for l in seentence.split("\n")
+  for l in sentence.split("\n"):
     block = l.split("\t")
-    word_list.append(block)
+    word_list.append(block[0])
   return word_list  
+
+def main(): 
+  sentence = "今日は仕事をした。"
+  analysed_sentence = morphological_analysis_default(sentence)
+  print(analysed_sentence)
+  print(clean_mecab_sentence(analysed_sentence))
+
+main()
