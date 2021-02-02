28c33a7504754704a6008dfde43c47141e04bf88
Yutaro Nakayama <170808naka@gmail.com>
2021-02-02 14:16:56 +0900

    modify freaquency_analysis.py

--- src/frequency_analysis.py
+++ src/frequency_analysis.py
@@ -4,11 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 test_path = "data/bluesky_restart"
-test_word_class_list = ["名詞","動詞","形容詞"]
 word_class_list = ["名詞","動詞","形容詞","形容動詞","副詞","連体詞","接続詞","感動詞","助動詞","助詞"]
 
 #最頻出トップ20ワード
-def most_words(text_path: str = test_path) -> str:
+def most_words(text_path: str = test_path, word_num: int = 20) -> str:
   #ファイルの書き込み用の指定
   f = open(text_path)
   text = f.read() 
@@ -16,25 +15,23 @@ def most_words(text_path: str = test_path) -> str:
   words = []
   #文章の分かち書き
   mt = MeCab.Tagger()
-  node = mt.parse(text)
-  #print(node) 
+  node = mt.parseToNode(text)
+  
   #品詞の分類
   while node:
-    word_class = node.feature.split(",")[4]
-    print("品詞名: " + word_class)
-    if word_class in test_word_class_list:
-      origin = node.feature.split(",")[0]
+    #単語要素が入っているか判断 
+    word_class = node.feature.split(",")[0]
+    #品詞リストにword_classの品詞が含まれていた場合の処理 
+    if word_class in word_class_list:
+      if len(node.feature.split(",")) < 8:
+        pass
+      else:
+        origin = node.feature.split(",")[8]
       words.append(origin)
     node = node.next
   #品詞別の集計  
   col = collections.Counter(words)
-  most_words = col.most_common(20)
-  print(most_words)
-   
-  #グラフへの出力
-  sns.set(context="talk")
-  fig = plt.subplots(figsize=(8, 8))
-  sns.countplot(y=words, order=[i[0] for i in col.most_common(20)])
+  most_words = col.most_common(word_num)
 
   #ファイルオープンの解放
   f.close()
@@ -42,6 +39,5 @@ def most_words(text_path: str = test_path) -> str:
   return most_words 
 
 def main():
-  print(most_words(test_path))
+  print(most_words(test_path,100))
 
-main()
